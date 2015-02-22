52392c900e02fc92336e34a1f800434462d5a15d
waten <waten1992@gmail.com>
2015-02-22 18:08:43 +0800

    增加删除节点递归注释

--- Bin_C++_Constr.cpp
+++ Bin_C++_Constr.cpp
@@ -142,7 +142,21 @@ void Insert(const Comparable & x , BinaryNode *& t)
 	else
 		;
 }
+/*
+remove(const Comparable &x , BinaryNode *&t)
+递归版本：
+低估出口：
+		if ( t == NULL ) return ;
+递归条件：
+		大于KEY 去右子树找 
+		小于KEY 去左子树找
+		等于KEY 
+				1-是叶子节点 ，直接删除
+				2-有2个子树时，找到当前节点的右子树的最小孩子代替当前节点，
+				再次调用Remove(),删除右孩子最小节点
+----------------------------------------------------
 
+*/
 void Remove(const Comparable &x , BinaryNode *& t)
 {
 	if ( t == NULL )
@@ -156,7 +170,7 @@ void Remove(const Comparable &x , BinaryNode *& t)
 		t->element = FindMin(t->right)->element;//找到右子树的最小孩子代替当前元素
 		Remove(t->element,t->right);//再次调用Remove()
 	}
-	else
+	else // 当只有左子树 或者  右子树 的时候
 	{
 		BinaryNode *OldNode = t ; 
 		t = (t->left != NULL) ? t->left : t->right ; //若左子节点不为空，直接指向左子树。同理右子树也一样
